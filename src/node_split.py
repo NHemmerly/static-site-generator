854d05a8b2b3199d4d5103773d7b4720b8743657
Neil Hemmerly <neilhemm0@gmail.com>
2024-12-31 16:27:41 -0500

    complete untested split_nodes_images function

--- src/node_split.py
+++ src/node_split.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+from extract_markdown import extract_markdown_links, extract_markdown_images
 
 def node_delimiter_split(node_list, delim, type):
     new_nodes = []
@@ -15,5 +16,25 @@ def node_delimiter_split(node_list, delim, type):
             if i % 2 == 0:
                 new_nodes.append(TextNode(section, TextType.TEXT))
             else: new_nodes.append(TextNode(section, type))
-
     return new_nodes
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        text = node.text
+        images = extract_markdown_images(text)
+        for image in images:
+            substring = f"![{image[0]}]({image[1]})"
+            sections = text.split(substring, 1)
+            for i, section in enumerate(sections):
+                if i % 2 == 0:
+                    if section == "":
+                        continue
+                    new_nodes.append(TextNode(section, TextType.TEXT))
+                else: 
+                    new_nodes.append(TextNode(image[0], TextType.IMAGE, image[1]))
+                if len(sections) > 1:
+                    text = sections[1]
+                else: text = ""
+
+    print(new_nodes)
